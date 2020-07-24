4a78af21bbd463b698443875ad0381e9a748d6e6
Mike Schwartz <schwartzmpnyc@gmail.com>
2020-07-24 14:24:44 -0400

    initial parcel exploration

--- src/contentScripts/mainPage.js
+++ src/contentScripts/mainPage.js
@@ -1,135 +1,126 @@
-const getTextFromSelectorIfExists = (selector, wrapper = document) => wrapper.querySelector(selector)?.textContent;
+import { getBadgeText } from '../utils/badgeUtils.js';
+
+const getTextFromSelectorIfExists = (selector, wrapper = document) =>
+    wrapper.querySelector(selector)?.textContent;
 
 const setLabel = (el, label) => el.setAttribute('aria-label', label);
 
 const getPriceFromCapsule = capsule => {
-	const finalPrice = getTextFromSelectorIfExists('.discount_final_price', capsule);
-	const discountPercent = getTextFromSelectorIfExists('.discount_pct', capsule);
-	const orginalPrice = getTextFromSelectorIfExists('.discount_original_price', capsule);
+    const finalPrice = getTextFromSelectorIfExists('.discount_final_price', capsule);
+    const discountPercent = getTextFromSelectorIfExists('.discount_pct', capsule);
+    const orginalPrice = getTextFromSelectorIfExists('.discount_original_price', capsule);
 
-	const addDataAndLabelToStringIfAvail = (label, data) => (data ? `${label}: ${data}. ` : '');
+    const addDataAndLabelToStringIfAvail = (label, data) => (data ? `${label}: ${data}. ` : '');
 
-	return (
-		addDataAndLabelToStringIfAvail('Price', finalPrice) +
-		addDataAndLabelToStringIfAvail('Discount', discountPercent) +
-		addDataAndLabelToStringIfAvail('Original price', orginalPrice)
-	);
+    return (
+        addDataAndLabelToStringIfAvail('Price', finalPrice) +
+        addDataAndLabelToStringIfAvail('Discount', discountPercent) +
+        addDataAndLabelToStringIfAvail('Original price', orginalPrice)
+    );
 };
 
 const getTitle = async capsule => {
-	const { dataset } = capsule;
-	const href = capsule.href || capsule.querySelector('a').href;
-	let path;
+    const { dataset } = capsule;
+    const href = capsule.href || capsule.querySelector('a').href;
+    let path;
 
-	if (dataset.dsBundleid) {
-		path = `bundle/${dataset.dsBundleid}/hover_public`;
-	} else if (dataset.dsPackageid) {
-		path = `subhoverpublic/${dataset.dsPackageid}`;
-	} else {
-		path = `apphoverpublic/${dataset.dsAppid}`;
-	}
+    if (dataset.dsBundleid) {
+        path = `bundle/${dataset.dsBundleid}/hover_public`;
+    } else if (dataset.dsPackageid) {
+        path = `subhoverpublic/${dataset.dsPackageid}`;
+    } else {
+        path = `apphoverpublic/${dataset.dsAppid}`;
+    }
 
-	const url = `https://store.steampowered.com/${path}`;
+    const url = `https://store.steampowered.com/${path}`;
 
-	const fetchTitleFromUrl = async (constructedUrl, backupHref) =>
-		await fetch(constructedUrl)
-			// If there isn't a hover for appid, then we need to load the link and get the page title
-			.then(async response => (await response.text()) || (await fetch(backupHref).then(response => response.text())))
-			.then(text => document.createRange().createContextualFragment(text))
-			.then(fragment => getTextFromSelectorIfExists('.hover_title', fragment) || getTextFromSelectorIfExists('title', fragment));
+    const fetchTitleFromUrl = async (constructedUrl, backupHref) =>
+        await fetch(constructedUrl)
+            // If there isn't a hover for appid, then we need to load the link and get the page title
+            .then(
+                async response =>
+                    (await response.text()) || (await fetch(backupHref).then(response => response.text()))
+            )
+            .then(text => document.createRange().createContextualFragment(text))
+            .then(
+                fragment =>
+                    getTextFromSelectorIfExists('.hover_title', fragment) ||
+                    getTextFromSelectorIfExists('title', fragment)
+            );
 
-	return fetchTitleFromUrl(url, href);
+    return fetchTitleFromUrl(url, href);
 };
 
 const labelStoreCapsules = async () => {
-	document.querySelectorAll('a:not(.gutter_item)[data-ds-appid], a:not(.gutter_item)[data-ds-packageid], a:not(.gutter_item)[data-ds-bundleid]').forEach(capsule => {
-		getTitle(capsule)
-			.then(title => {
-				if (title) {
-					const price = getPriceFromCapsule(capsule);
-					const platforms = [...capsule.querySelectorAll('.platform_img')].map(getBadgeText).join(', ');
-					const topTags = [...capsule.querySelectorAll('.top_tag')].map(tag => tag.textContent).join('');
-					const vrOnly = capsule.querySelector('.vr_required');
+    document
+        .querySelectorAll(
+            'a:not(.gutter_item)[data-ds-appid], a:not(.gutter_item)[data-ds-packageid], a:not(.gutter_item)[data-ds-bundleid]'
+        )
+        .forEach(capsule => {
+            getTitle(capsule)
+                .then(title => {
+                    if (title) {
+                        const price = getPriceFromCapsule(capsule);
+                        const platforms = [...capsule.querySelectorAll('.platform_img')]
+                            .map(getBadgeText)
+                            .join(', ');
+                        const topTags = [...capsule.querySelectorAll('.top_tag')]
+                            .map(tag => tag.textContent)
+                            .join('');
+                        const vrOnly = capsule.querySelector('.vr_required');
 
-					setLabel(
-						capsule,
-						`${title}${price ? ': ' + price : ''}${vrOnly ? ' VR Only.' : ''}${platforms.length ? ' Platforms: ' + platforms + '.' : ''}${
-							topTags.length ? ' Top tags: ' + topTags + '.' : ''
-						}`
-					);
-				}
-			})
-			.catch(console.error);
-	});
+                        setLabel(
+                            capsule,
+                            `${title}${price ? ': ' + price : ''}${vrOnly ? ' VR Only.' : ''}${
+                                platforms.length ? ' Platforms: ' + platforms + '.' : ''
+                            }${topTags.length ? ' Top tags: ' + topTags + '.' : ''}`
+                        );
+                    }
+                })
+                .catch(console.error);
+        });
 };
 
 const labelSpotLights = async () => {
-	for (const spotlight of document.querySelectorAll(':not(a)[data-ds-appid], :not(a)[data-ds-packageid], :not(a)[data-ds-bundleid]')) {
-		const link = spotlight.querySelector('a');
-		const price = getPriceFromCapsule(spotlight);
-		const spotlightHeader = spotlight.querySelector('.spotlight_content h2');
+    for (const spotlight of document.querySelectorAll(
+        ':not(a)[data-ds-appid], :not(a)[data-ds-packageid], :not(a)[data-ds-bundleid]'
+    )) {
+        const link = spotlight.querySelector('a');
+        const price = getPriceFromCapsule(spotlight);
+        const spotlightHeader = spotlight.querySelector('.spotlight_content h2');
 
-		getTitle(spotlight)
-			.then(title => {
-				setLabel(link, `${title}: ${spotlightHeader.textContent}.${price ? ' ' + price : ''}`);
-				// link.setAttribute('aria-label', `${title}: ${spotlightHeader.textContent}.${price ? ' ' + price : ''}`);
-			})
-			.catch(console.error);
-	}
-};
-
-// TODO: remove this and import from badges.js
-const getBadgeText = badge => {
-	let platform = badge.getAttribute('title');
-
-	if (!platform)
-		switch (badge.classList[1]) {
-			case 'win':
-				platform = 'Windows';
-				break;
-			case 'linux':
-				platform = 'Linux';
-				break;
-			case 'mac':
-				platform = 'Mac';
-				break;
-			case 'valveindex':
-				platform = 'Valve Index';
-				break;
-			case 'htcvive':
-				platform = 'HTC Vive';
-				break;
-			case 'oculusrift':
-				platform = 'Oculus Rift';
-				break;
-			case 'windowsmr':
-				platform = 'Windows Mixed Reality';
-				break;
-		}
-
-	return platform;
+        getTitle(spotlight)
+            .then(title => {
+                setLabel(link, `${title}: ${spotlightHeader.textContent}.${price ? ' ' + price : ''}`);
+                // link.setAttribute('aria-label', `${title}: ${spotlightHeader.textContent}.${price ? ' ' + price : ''}`);
+            })
+            .catch(console.error);
+    }
 };
 
 const labelweekLongDealsIdentifier = () => {
-	// TODO: See if we need to aria-hide the new deals each week div to keep it from repeating within the link.
-	document
-		.querySelectorAll('.spotlight_weeklong_ctn')
-		.forEach(weekLongDealsIdentifier =>
-			setLabel(
-				weekLongDealsIdentifier.parentNode,
-				`Week long deals: ${weekLongDealsIdentifier.querySelector('.spotlight_count').textContent} on sale this week. ${
-					weekLongDealsIdentifier.parentNode.querySelector('.spotlight_weeklong_subtitle')?.textContent
-				}`
-			)
-		);
+    // TODO: See if we need to aria-hide the new deals each week div to keep it from repeating within the link.
+    document
+        .querySelectorAll('.spotlight_weeklong_ctn')
+        .forEach(weekLongDealsIdentifier =>
+            setLabel(
+                weekLongDealsIdentifier.parentNode,
+                `Week long deals: ${
+                    weekLongDealsIdentifier.querySelector('.spotlight_count').textContent
+                } on sale this week. ${
+                    weekLongDealsIdentifier.parentNode.querySelector('.spotlight_weeklong_subtitle')
+                        ?.textContent
+                }`
+            )
+        );
 };
 
 const accessify = async () => {
-	console.log('Accessifying Steam');
-	labelweekLongDealsIdentifier();
-	labelSpotLights();
-	labelStoreCapsules();
-	console.log('Accessified Steam');
+    console.log('Accessifying Steam');
+    labelweekLongDealsIdentifier();
+    labelSpotLights();
+    labelStoreCapsules();
+    console.log('Accessified Steam');
 };
 
 // Steam has some events firing during load that end up messing with data attributes we need to make this work,
---
+++ src/utils/badgeUtils.js
@@ -0,0 +1,47 @@
+const labelPlatformBadges = () => {
+    document.querySelectorAll('.platform_img').forEach(badge => {
+        // If we make these into a list in some places, these are the wrapper classes for the parents.
+        // Unfortunately, not everywhere has one:
+        // game_area_purchase_platform
+        // platform
+
+        const platform = getBadgeText(badge);
+        if (platform) setLabel(badge, platform);
+    });
+};
+
+const getBadgeText = badge => {
+    let platform = badge.getAttribute('title');
+
+    if (!platform)
+        switch (badge.classList[1]) {
+            case 'win':
+                platform = 'Windows';
+                break;
+            case 'linux':
+                platform = 'Linux';
+                break;
+            case 'mac':
+                platform = 'Mac';
+                break;
+            case 'valveindex':
+                platform = 'Valve Index';
+                break;
+            case 'htcvive':
+                platform = 'HTC Vive';
+                break;
+            case 'oculusrift':
+                platform = 'Oculus Rift';
+                break;
+            case 'windowsmr':
+                platform = 'Windows Mixed Reality';
+                break;
+        }
+
+    return platform;
+};
+
+exports = {
+    labelPlatformBadges,
+    getBadgeText
+};
