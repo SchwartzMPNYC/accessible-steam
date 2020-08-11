550be9111f333c2583d697a4d313a47dc3d1044a
Mike Schwartz <schwartzmpnyc@gmail.com>
2020-08-11 17:07:07 -0400

    Badge utils formatting

--- src/utils/badgeUtils.js
+++ src/utils/badgeUtils.js
@@ -1,44 +1,48 @@
-const labelPlatformBadges = () => {
-    document.querySelectorAll('.platform_img').forEach(badge => {
-        // If we make these into a list in some places, these are the wrapper classes for the parents.
-        // Unfortunately, not everywhere has one:
-        // game_area_purchase_platform
-        // platform
+import { setLabel } from './setLabel.js';
 
-        const platform = getBadgeText(badge);
-        if (platform) setLabel(badge, platform);
-    });
+const labelPlatformBadges = () => {
+	document.querySelectorAll('.platform_img').forEach(badge => {
+		// If we make these into a list in some places, these are the wrapper classes for the parents.
+		// Unfortunately, not everywhere has one:
+		// game_area_purchase_platform
+		// platform
+
+		const platform = getBadgeText(badge);
+
+		// console.log(badge, platform);
+		if (platform) setLabel(badge, platform);
+	});
 };
 
 const getBadgeText = badge => {
-    let platform = badge.getAttribute('title');
+	let platform = badge.getAttribute('title');
 
-    if (!platform)
-        switch (badge.classList[1]) {
-            case 'win':
-                platform = 'Windows';
-                break;
-            case 'linux':
-                platform = 'Linux';
-                break;
-            case 'mac':
-                platform = 'Mac';
-                break;
-            case 'valveindex':
-                platform = 'Valve Index';
-                break;
-            case 'htcvive':
-                platform = 'HTC Vive';
-                break;
-            case 'oculusrift':
-                platform = 'Oculus Rift';
-                break;
-            case 'windowsmr':
-                platform = 'Windows Mixed Reality';
-                break;
-        }
+	if (!platform)
+		switch (badge.classList[1]) {
+			case 'win':
+				platform = 'Windows';
+				break;
+			case 'linux':
+				platform = 'Linux';
+				break;
+			case 'mac':
+				platform = 'Mac';
+				break;
+			case 'valveindex':
+				platform = 'Valve Index';
+				break;
+			case 'htcvive':
+				platform = 'HTC Vive';
+				break;
+			case 'oculusrift':
+				platform = 'Oculus Rift';
+				break;
+			case 'windowsmr':
+				platform = 'Windows Mixed Reality';
+				break;
+		}
 
-    return platform;
+	return platform;
 };
 
 export { labelPlatformBadges, getBadgeText };
