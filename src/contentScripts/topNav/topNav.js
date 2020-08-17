bbdae7c82be2ec213a0ad49d55e1d56b89837700
Mike Schwartz <schwartzmpnyc@gmail.com>
2020-08-17 15:26:40 -0400

    Added code to label top nav and assign aria role. Just put directly in the top nav file, because it's so little code that it felt like splitting it into it's own partial was needlessly complicating things. If I realize that more has to be done with this, then that may change.

--- src/contentScripts/topNav/topNav.js
+++ src/contentScripts/topNav/topNav.js
@@ -1,5 +1,11 @@
 import { superNav } from './_superNav.js';
 import { languageDropdown } from './_languageDropdown.js';
+import { setLabel } from '../../utils/setLabel.js';
+
+const globalHeader = document.querySelector('#global_header');
+globalHeader.setAttribute('role', 'navigation');
+// TODO: Come up with a better name for AT
+setLabel(globalHeader, 'Top level navigation');
 
 superNav();
 languageDropdown();
